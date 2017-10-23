475deb45cfb54a54322a18fc8aa50a1bdb42f847
Serhiy Ver <4bugzilla@gmail.com>
2017-10-23 14:00:57 -0600

    Enable Hystrix

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,8 +1,13 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ProjectClient {
+
+    private ConcurrentHashMap<Long, ProjectInfo> cache = new ConcurrentHashMap<>();
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
@@ -12,7 +17,15 @@ public class ProjectClient {
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo info = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, info);
+        return info;
+    }
+
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cache.get(projectId);
     }
 }
