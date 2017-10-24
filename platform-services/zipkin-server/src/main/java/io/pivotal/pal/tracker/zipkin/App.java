6c963e65c7bd6db342bc31116508fc4ec09cf0b0
Serhiy Ver <4bugzilla@gmail.com>
2017-10-24 11:23:55 -0600

    Configure zipkin

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,20 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.ComponentScan;
+import zipkin.server.EnableZipkinServer;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@ComponentScan({"io.pivotal.pal.tracker.zipkin"})
+@EnableZipkinStreamServer
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
